7a62a5458a1880f2af1d76cab5feac977bc5df8e
Anil Virdee <avirdee0720@yahoo.co.uk>
2017-09-13 20:04:15 +0100

    created grid and tested div box classes

---
+++ main.js
@@ -0,0 +1,44 @@
+currentPlayer = 'x';
+
+$('.box1').on('click', function (event) {
+	console.log('box1');
+});
+
+$('.box2').on('click', function (event) {
+	console.log('box2');
+
+});
+
+$('.box3').on('click', function (event) {
+	console.log('box3');
+
+});
+
+$('.box4').on('click', function (event) {
+	console.log('box4');
+
+});
+
+$('.box5').on('click', function (event) {
+	console.log('box5');
+
+});
+
+$('.box6').on('click', function (event) {
+	console.log('box6');
+
+});
+
+$('.box7').on('click', function (event) {
+	console.log('box7');
+
+});
+
+$('.box8').on('click', function (event) {
+	console.log('box8');
+
+});
+$('.box9').on('click', function (event) {
+	console.log('box9');
+
+});
