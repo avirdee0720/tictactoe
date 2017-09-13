44284288c9b126e43c9ab71c3775644414c27b67
Anil Virdee <avirdee0720@yahoo.co.uk>
2017-09-13 20:56:44 +0100

    added swap player function and works

--- main.js
+++ main.js
@@ -1,52 +1,61 @@
-var currentPlayer = 'x';
+var currentPlayer = 'X';
 
 $('.box1').on('click', function (event) {
 	// console.log(currentPlayer);
 	$('.box1').html(currentPlayer);
+	swapPlayers();
 });
 
 $('.box2').on('click', function (event) {
 	$('.box2').html(currentPlayer);
+	swapPlayers();
 
 });
 
 $('.box3').on('click', function (event) {
 	$('.box3').html(currentPlayer);
+	swapPlayers();
 
 });
 
 $('.box4').on('click', function (event) {
 	$('.box4').html(currentPlayer);
-
+	swapPlayers();
 });
 
 $('.box5').on('click', function (event) {
 	$('.box5').html(currentPlayer);
-
+	swapPlayers();
 });
 
 $('.box6').on('click', function (event) {
 	$('.box6').html(currentPlayer);
-
+	swapPlayers();
 });
 
 $('.box7').on('click', function (event) {
 	$('.box7').html(currentPlayer);
-
+	swapPlayers();
 });
 
 $('.box8').on('click', function (event) {
 	$('.box8').html(currentPlayer);
-
+	swapPlayers();
 });
+
 $('.box9').on('click', function (event) {
 	$('.box9').html(currentPlayer);
+	swapPlayers();
 
 });
 
-// function swapPlayers() {
-
-// }
+function swapPlayers() {
+	if (currentPlayer === 'X') {
+		currentPlayer = 'O';
+	} else {
+		currentPlayer = 'X';
+	}
+}
 
 // function checkPattern {
 
